8beaba7b54d1aebcbc8c68af62a803d5e9b43431
Amrish Kumar <ak3754732@gmail.com>
2023-01-27 01:34:46 +0530

    server file updated

--- server.js
+++ server.js
@@ -9,14 +9,18 @@ mongoose.set("strictQuery", false);
 app.use(cors());
 app.use(express.json());
 
+const DB =
+  "mongodb+srv://netflix:netflix321@cluster0.sc1dvgn.mongodb.net/netflix-app?retryWrites=true&w=majority";
 mongoose
-  .connect("mongodb://127.0.0.1:27017/netflix", {
+  .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
     console.log("mongodb connected");
-  });
+  }).catch(()=> {
+    console.log('mongodb not connected');
+  })
 
 app.use("/api/user", userRoutes);
 
